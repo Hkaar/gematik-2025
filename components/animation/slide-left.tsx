9ca16295cfa20950082e347c710c3c885dbf41e7
Hkaar <HkaarDev@gmail.com>
2025-11-17 15:46:13 +0800

    added animation components

---
+++ components/animation/slide-left.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+import * as motion from "framer-motion/client";
+
+interface Props extends React.HTMLAttributes<HTMLElement> {
+  delay?: number;
+  repeat?: boolean;
+}
+
+export default function SlideLeft({
+  delay,
+  repeat,
+  children,
+  className,
+}: Props) {
+  return (
+    <motion.div
+      className={cn("w-full", className)}
+      initial={{ x: 100, opacity: 0, z: 30 }}
+      whileInView={{
+        x: 0,
+        opacity: 1,
+        z: 0,
+        transition: { duration: delay ? delay : 1.2 },
+      }}
+      viewport={{ once: repeat ? false : true }}
+    >
+      {children}
+    </motion.div>
+  );
+}
